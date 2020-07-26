19f36d34d6423fe2e04a845b8cba8735398a1127
TharinduDeshananda <48414771+TharinduDeshananda@users.noreply.github.com>
2020-07-26 23:26:53 +0530

    Delete compile.bat

--- source/bootload/compile.bat
+++
@@ -1,2 +0,0 @@
-
-"C:\Users\Ruwan Bandara\AppData\Local\bin\NASM\nasm.exe" -f bin "C:\Users\Ruwan Bandara\Desktop\New folder (2)\Aqua_Seven_OS-master\AQUASEVEN\source\bootload\bootload.asm" -o"C:\Users\Ruwan Bandara\Desktop\New folder (2)\Aqua_Seven_OS-master\AQUASEVEN\source\bootload\bootload.iso"
